e74b1d60533b0dbb82e581a88bbd329611e84701
lin <Cython_lin@cklin.top>
2022-09-24 16:43:45 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+setup(
+    name='bdtool',
+    version="1.2.0",
+    author="linusic",
+    author_email='cython_lin@cklin.top',
+    description="script of managing some bigdata technology stack",
+    url="https://github.com/linusic/bdtool",   # Github Repositoriy (auto sync stars ...)
+    classifiers=[
+        'Intended Audience :: Developers',        
+        "Environment :: Console",                 
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],  
+
+    entry_points={ 
+        "console_scripts": [ 
+            "fa=bdtool:main" # bdtool.py -> def main
+        ]
+    },
+    py_modules=["bdtool"], 
+    python_requires='>=3.6',
+)
